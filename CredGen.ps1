340cac6c9114e1a71c5b1b191bc54fb9d5af8b94
terriens <54020344+terriens@users.noreply.github.com>
2019-08-12 12:38:29 +0200

    Update CredGen.ps1

--- CredGen.ps1
+++ CredGen.ps1
@@ -1,4 +1,9 @@
-﻿function New-KeyGenAES
+﻿
+ 
+$User = "Domain\ServiceAdministrator"
+$PasswordFile = "c:\DataInformation\Password.txt"
+$KeyFile = "c:\DataInformation\AES.key"
+function New-KeyGenAES
 {
     $KeyFile = "c:\DataInformation\AES.key"
     $Key = New-Object Byte[] 16   # You can use 16, 24, or 32 for AES
@@ -10,12 +15,9 @@ function New-KeyGen
     $PasswordFile = "c:\DataInformation\Password.txt"
     $KeyFile = "c:\DataInformation\AES.key"
     $Key = Get-Content $KeyFile
-    $Password = "3tral1P@ss2CLOUD" | ConvertTo-SecureString -AsPlainText -Force
+    $Password = "S3pVr_P@ssw0rd!!" | ConvertTo-SecureString -AsPlainText -Force
     $Password | ConvertFrom-SecureString -key $Key | Out-File $PasswordFile
 }
-$User = "CLOUD\Administrator"
-$PasswordFile = "c:\DataInformation\Password.txt"
-$KeyFile = "c:\DataInformation\AES.key"
 $key = Get-Content $KeyFile
 $MyCredential = New-Object -TypeName System.Management.Automation.PSCredential `
  -ArgumentList $User, (Get-Content $PasswordFile | ConvertTo-SecureString -Key $key)
